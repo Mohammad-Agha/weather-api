2450f10b17dea5fcbfacf7d8eb7e30c1182bbe5e
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-13 22:05:38 +0300

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
-import React, { Component } from "react";
-import Search from "./components/Search";
-
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
-
-import "./App.css";
+import React, { Component } from "react"
+import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Mohammad"
-    };
+  constructor() {
+    super();
+    this.state = {}
   }
-
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
 
   render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+      <div>
+        <header>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Type in a city name" />
+            <button className="btn">FIND WEATHER</button>
+          </div>
+        </header>
+        <main>
+          <img className="main-weather" src="https://placekitten.com/150/150" />
+          <p className="weather-status">overcast clouds</p>
+          <p className="weather-temp"><span className="bolder">Temperature</span> <span className="from">10&deg;</span> to <span className="to">11&deg;</span>C</p>
+          <p className="humidity-pressure"><span className="bolder">Humidity</span> <span className="hum">78%</span> <span className="bolder">Pressure</span>  <span className="press">1008.48</span></p>
+          <div className="h-weather">
+            <div className="h-weather-item">
+              <span className="h-time">03:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">8&deg;C</span>
+            </div>
+            <div className="h-weather-item">
+              <span className="h-time">06:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">9&deg;C</span>
+            </div><div className="h-weather-item">
+              <span className="h-time">09:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">14&deg;C</span>
+            </div><div className="h-weather-item">
+              <span className="h-time">12:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">17&deg;C</span>
+            </div><div className="h-weather-item">
+              <span className="h-time">15:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">18&deg;C</span>
+            </div><div className="h-weather-item">
+              <span className="h-time">18:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">16&deg;C</span>
+            </div><div className="h-weather-item">
+              <span className="h-time">21:00</span>
+              <img className="h-weather-img" src="https://placekitten.com/80/80" />
+              <span className="h-weather-temp">13&deg;C</span>
+            </div>
+          </div>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
