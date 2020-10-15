2e1d974c8652c4bd9b8ba467200797f240c1e389
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-15 12:16:43 +0300

    changed classes to class based components

--- src/components/HWeather.js
+++ src/components/HWeather.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HWeatherItem from './HWeatherItem'
 import clear from "../img/weather-icons/clear.svg";
 import drizzle from "../img/weather-icons/drizzle.svg";
@@ -10,50 +10,59 @@ import snow from "../img/weather-icons/snow.svg";
 import storm from "../img/weather-icons/storm.svg";
 import unknown from "../img/weather-icons/unknown.svg";
 
-export default function HWeather(props) {
-    const nextHours = props.weather.map(weather => {
-        const id = weather.weather[0].id
-        let img;
-        if (id < 300) {
-            img = storm
-        }
-        else if (id < 500) {
-            img = drizzle
-        }
-        else if (id < 600) {
-            img = rain
-        }
-        else if (id < 700) {
-            img = snow
-        }
-        else if (id < 800) {
-            img = fog
-        }
-        else if (id === 800) {
-            img = clear
-        }
-        else if (id === 801) {
-            img = partlycloudy
-        }
-        else if (id < 806) {
-            img = mostlycloudy
-        }
-        else {
-            img = unknown
-        }
+class HWeather extends Component {
+    constructor(props) {
+        super(props)
+        this.setState = {}
+    }
 
+    render() {
+        const nextHours = this.props.weather.map(weather => {
+            const id = weather.weather[0].id
+            let img;
+            if (id < 300) {
+                img = storm
+            }
+            else if (id < 500) {
+                img = drizzle
+            }
+            else if (id < 600) {
+                img = rain
+            }
+            else if (id < 700) {
+                img = snow
+            }
+            else if (id < 800) {
+                img = fog
+            }
+            else if (id === 800) {
+                img = clear
+            }
+            else if (id === 801) {
+                img = partlycloudy
+            }
+            else if (id < 806) {
+                img = mostlycloudy
+            }
+            else {
+                img = unknown
+            }
+
+            return (
+                <HWeatherItem
+                    key={Math.random()}
+                    time={weather.dt_txt.substr(11, 5)}
+                    temp={Math.floor(weather.main.temp)}
+                    img={img}
+                />
+            )
+        })
         return (
-            <HWeatherItem
-                key={Math.random()}
-                time={weather.dt_txt.substr(11, 5)}
-                temp={Math.floor(weather.main.temp)}
-                img={img}
-            />
+            <div className="h-weather">
+                {nextHours}
+            </div>
         )
-    })
-    return (
-        <div className="h-weather">
-            {nextHours}
-        </div>
-    )
+    }
 }
+
+export default HWeather
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HWeather from './HWeather';
 import MainWeather from './MainWeather';
 import Loading from '../img/loading.gif'
-export default function Main(props) {
-    if (props.buttonClicked) {
-        if (props.loading) {
-            return <main><img src={Loading} alt="Loading" /></main>
-        }
 
-        if (props.error) {
-            return <main>City was not found</main>
-        }
-
-        else {
-            let res = []
-            props.weathers.forEach(item => {
-                let now = new Date();
-                let time = new Date(item.dt_txt)
-                res.push({
-                    main: item.main,
-                    diff: Math.abs(now - time),
-                    id: item.weather[0].id,
-                    description: item.weather[0].description
-                })
-            })
-            let obj = res[0];
-            res.forEach(item => {
-                if (item.diff <= obj.diff) {
-                    obj = item
-                }
-            })
-            return (
-                <main>
-                    <MainWeather weather={obj} />
-                    <HWeather weather={props.weathers} />
-                </main>
-            )
-        }
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
     }
-    return (
-        <main></main>
-    )
 
+    render() {
+        if (this.props.buttonClicked) {
+            if (this.props.loading) {
+                return <main><img src={Loading} alt="Loading" /></main>
+            }
+
+            if (this.props.error) {
+                return <main>City was not found</main>
+            }
+
+            else {
+                let res = []
+                this.props.weathers.forEach(item => {
+                    let now = new Date();
+                    let time = new Date(item.dt_txt)
+                    res.push({
+                        main: item.main,
+                        diff: Math.abs(now - time),
+                        id: item.weather[0].id,
+                        description: item.weather[0].description
+                    })
+                })
+                let obj = res[0];
+                res.forEach(item => {
+                    if (item.diff <= obj.diff) {
+                        obj = item
+                    }
+                })
+                return (
+                    <main>
+                        <MainWeather weather={obj} />
+                        <HWeather weather={this.props.weathers} />
+                    </main>
+                )
+            }
+        }
+        return (
+            <main></main>
+        )
+    }
 }
+
+export default Main
